bb196226dfa90d877fde496c7f7a92b2585125fa
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-17 02:44:02 -0500

    Creación de carpetas para colocar los casos de uso dentro de la capa de dominio

--- domain/src/main/java/com/mona15/domain/country/exceptions/TechnicalException.kt
+++
@@ -1,5 +0,0 @@
-package com.mona15.domain.country.exceptions
-
-private const val TECHNICAL_EXCEPTION_CODE = 500
-
-class TechnicalException (val codeMessage: Int = TECHNICAL_EXCEPTION_CODE) : Exception(codeMessage.toString())
