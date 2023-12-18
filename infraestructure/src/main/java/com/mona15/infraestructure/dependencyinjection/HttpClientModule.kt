7faef2b18566111cfd283b8190996bd0f0323660
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-18 09:44:33 -0500

    Cambio de url por un protocolo https seguro

--- infraestructure/src/main/java/com/mona15/infraestructure/dependencyinjection/HttpClientModule.kt
+++ infraestructure/src/main/java/com/mona15/infraestructure/dependencyinjection/HttpClientModule.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-private const val BASE_URL = "http://demo3790638.mockable.io/"
+private const val BASE_URL = "https://demo3790638.mockable.io/"
 
 @Module
 @InstallIn(SingletonComponent::class)
