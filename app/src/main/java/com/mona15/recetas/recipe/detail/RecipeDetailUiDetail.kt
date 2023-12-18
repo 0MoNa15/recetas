c498631dc0bbbc1ab9368ec9beee2018e8d4da24
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-18 10:10:38 -0500

    Limpieza de código para el uistate del detalle

--- app/src/main/java/com/mona15/recetas/recipe/detail/RecipeDetailUiDetail.kt
+++
@@ -1,8 +0,0 @@
-package com.mona15.recetas.recipe.detail
-
-import com.mona15.domain.recipe.model.RecipeDetail
-import com.mona15.recetas.BaseUiState
-
-data class RecipeDetailUiDetaildata (
-    var success: RecipeDetail
-) : BaseUiState()
