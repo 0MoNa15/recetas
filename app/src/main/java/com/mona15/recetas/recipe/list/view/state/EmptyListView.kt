b79b4a48ec00908e352b9db42b687b00fb348021
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-19 10:14:24 -0500

    Limpieza de código en imports

--- app/src/main/java/com/mona15/recetas/recipe/list/view/state/EmptyListView.kt
+++ app/src/main/java/com/mona15/recetas/recipe/list/view/state/EmptyListView.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.mona15.recetas.R
