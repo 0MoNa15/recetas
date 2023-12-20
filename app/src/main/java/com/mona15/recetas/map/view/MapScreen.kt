f38fa9b155cf54e34223d8a7d15263b0541243c2
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-20 09:09:15 -0500

    Creación del mapa e integración con google maps

--- app/src/main/java/com/mona15/recetas/map/view/MapScreen.kt
+++ app/src/main/java/com/mona15/recetas/map/view/MapScreen.kt
@@ -1,13 +1,15 @@
 package com.mona15.recetas.map.view
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import com.mona15.recetas.R
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import com.mona15.recetas.map.model.LocationParcelable
 
 @Composable
@@ -15,23 +17,21 @@ fun MapScreen(
     location: LocationParcelable?,
     popBackStack: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(dimensionResource(id = R.dimen.padding_24_dp))
-    ) {
-        if (location != null) {
-            Text(
-                text = "Ubicación: ${location.toString()}",
-                //style = TextStyle(fontSize = 20.sp)
-            )
-        } else {
-            Text(
-                text = "Ubicación no disponible",
-                //style = TextStyle(fontSize = 20.sp)
-            )
-        }
 
-        // Agrega más contenido o interacciones según sea necesario
+    val locationMarker = LatLng(location!!.latitude, location.longitude)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(locationMarker, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+
+        Marker(
+            state = rememberMarkerState(position = locationMarker),
+            title = location.city,
+            snippet = location.countryParcelable.name,
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
+        )
     }
 }
